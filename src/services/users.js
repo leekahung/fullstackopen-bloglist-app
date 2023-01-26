51301ad42abe5c24f75fefc03ca7e46b12432f35
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-26 02:23:38 -0800

    Correcting routing to relative routing for deployment

--- src/services/users.js
+++ src/services/users.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8888/api/users";
+const baseUrl = "/api/users";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
